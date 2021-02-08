f4d3f6ce0e5d7eb1d4e214aa0bb4852866065c32
Bruce Gordon <brc.gordon@gmail.com>
2021-02-08 16:57:14 -0700

    Create Survey component

--- code/web/src/modules/survey/Question.js
+++ code/web/src/modules/survey/Question.js
@@ -1,7 +1,5 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import surveyData from './surveyData'
 
 class Question extends PureComponent {
 
