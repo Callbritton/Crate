2a30a037a8f916c079509165efa0c97495d18fd9
Chris Allbritton <cla.inbox@gmail.com>
2021-02-10 19:16:39 -0500

    Replace 5-style migration with 5-styles migration
    Co-authored-by: George Soderholm <62966396+GeorgieGirl24@users.noreply.github.com>
    Co-authored-by: Sage Freeman-Gonzales <65799246+SageOfCode@users.noreply.github.com>

---
+++ code/api/src/migrations/5-styles.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('styles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('styles');
+  }
+}
