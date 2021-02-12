252d017f191008db5d69800caf8969eaa6bfe83b
George Soderholm <georgesoderholm@gmail.com>
2021-02-11 20:25:23 -0700

    Add more styles to seed the db

--- code/api/src/seeders/4-styles.js
+++ code/api/src/seeders/4-styles.js
@@ -9,6 +9,24 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      {
+        description: 'Edgy',
+        image_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F489625790723093994%2F&psig=AOvVaw2_PuwxWPbaylVj9Hz3chSo&ust=1613186116255000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjfp9-w4-4CFQAAAAAdAAAAABAD',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        description: 'Casual',
+        image_url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.puttingmetogether.com%2Fwp-content%2Fuploads%2F2019%2F05%2FWhite-tee-brown-cardigan-skinny-jeans-leopard-print-sandals-grey-purse.jpg&imgrefurl=https%3A%2F%2Fwww.puttingmetogether.com%2Fways-to-wear-a-white-tee-spring-summer%2Fwhite-tee-brown-cardigan-skinny-jeans-leopard-print-sandals-grey-purse%2F&tbnid=7-9xvdw2-_i4nM&vet=10CAcQxiAoB2oXChMIqK6jx7Hj7gIVAAAAAB0AAAAAECY..i&docid=Q5yPqihOMoDX-M&w=3456&h=5184&itg=1&q=casual%20outfits&hl=en&client=safari&ved=0CAcQxiAoB2oXChMIqK6jx7Hj7gIVAAAAAB0AAAAAECY',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        description: 'Hipster',
+        image_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F355221489354115340%2F&psig=AOvVaw2pFsohN3LbHSXFusXXZuoD&ust=1613186490825000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJj35o-y4-4CFQAAAAAdAAAAABAF',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
     ], {})
   },
 
