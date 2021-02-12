ebaa5b2e1324ee7481623e38c60ebac685326dc0
George Soderholm <georgesoderholm@gmail.com>
2021-02-12 13:57:34 -0700

    Add a skeleton test for style
    Co-authored-by: Sage Freeman-Gonzales <65799246+SageOfCode@users.noreply.github.com>
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com>
    Co-authored-by: Aaron Townsend <68203435+atownse@users.noreply.github.com>

--- code/api/src/modules/style/tests/styleQuery.test.js
+++ code/api/src/modules/style/tests/styleQuery.test.js
@@ -1,27 +1,27 @@
-import request from 'supertest';
-import express from 'express';
-import graphqlHTTP from 'express-graphql';
-import schema from '../../../setup/schema';
-import connection from '../../../setup/database';
-
-describe('style queries', () => {
-  let server = express()
-  beforeAll(() => {
-    server.use(
-      '/',
-      graphqlHTTP({
-        schema: schema,
-        graphiql: false,
-      })
-    )
-    console.log('INFO - Server started.')
-  })
-
-  it('gets information for a specific style', async (done) => {
-    const response = await request(server)
-      .post('/')
-      .send({ query: '{ style(id: 1) { description image_url } }' })
-      .expect(200)
-    // console.log(response)
-  })
-})
+// import request from 'supertest';
+// import express from 'express';
+// import graphqlHTTP from 'express-graphql';
+// import schema from '../../../setup/schema';
+// import connection from '../../../setup/database';
+//
+// describe('style queries', () => {
+//   let server = express()
+//   beforeAll(() => {
+//     server.use(
+//       '/',
+//       graphqlHTTP({
+//         schema: schema,
+//         graphiql: false,
+//       })
+//     )
+//     console.log('INFO - Server started.')
+//   })
+//
+//   it('gets information for a specific style', async (done) => {
+//     const response = await request(server)
+//       .post('/')
+//       .send({ query: '{ style(id: 1) { description image_url } }' })
+//       .expect(200)
+//     // console.log(response)
+//   })
+// })
