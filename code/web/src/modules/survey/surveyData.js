4ed36671d603ff0606335ee2eee5eaf8f12320fe
Aaron Townsend <atownse17@gmail.com>
2021-02-13 17:49:43 -0600

    Add links for images Co-authored-by: Bruce Gordon <68293135+bruce-gordon@users.noreply.github.com> Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com> Co-authored-by: George Soderholm <5799246+SageOfCode@users.noreply.github.com> Co-authored-by: Sage Freeman-Gonzales <65799246+SageOfCode@users.noreply.github.com>

--- code/web/src/modules/survey/surveyData.js
+++ code/web/src/modules/survey/surveyData.js
@@ -5,22 +5,22 @@ export const surveyData = {
       title: 'Which vacation destination would you prefer?',
       choices: [
         {
-          image: "url",
+          image: "https://specials-images.forbesimg.com/imageserve/5e086a2f25ab5d0007cf74ec/960x0.jpg?cropX1=1&cropX2=1867&cropY1=0&cropY2=1244",
           label: "Hawaii",
           value: 1
         },
         {
-          image: "url",
+          image: "https://eartheclipse.b-cdn.net/wp-content/uploads/2019/01/andes-mountain-aconcagua-landscape.jpg",
           label: "Andes",
           value: 2
         },
         {
-          image: "url",
+          image: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/640/360/75/dam/disneyland/attractions/disneyland/sleeping-beauty-castle-walkthrough/sleeping-beauty-castle-exterior-16x9.jpg?1593556896598",
           label: "Disneyland",
           value: 3
         },
         {
-          image: "url",
+          image: "https://i.imgur.com/SVo6d3d.jpg",
           label: "Paris",
           value: 4
         }
@@ -31,22 +31,22 @@ export const surveyData = {
       title: 'Which home would you prefer?',
       choices: [
         {
-          image: "url",
+          image: "https://rentpath-res.cloudinary.com/w_336,h_280,t_rp,cs_tinysrgb,fl_force_strip,c_fill/e_unsharp_mask:50,q_auto/7d81e1b0c104f29a3de9826da132e985",
           label: "Downtown Apartment",
           value: 1
         },
         {
-          image: "url",
+          image: "https://cdn.liverez.com/5/11273/1/56757/800/1.jpg?v=12/4/2019%201:46:28%20PM",
           label: "Cabin",
           value: 2
         },
         {
-          image: "url",
+          image: "https://i.imgur.com/uB2YCaC.png",
           label: "Suburban House",
           value: 3
         },
         {
-          image: "url",
+          image: "https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2018/2/8/1/CI_Halstead_History-of-Penthouses-6.jpg.rend.hgtvcom.616.411.suffix/1518139442662.jpeg",
           label: "Penthouse",
           value: 4
         }
@@ -83,22 +83,22 @@ export const surveyData = {
       title: 'Where would you like to live?',
       choices: [
         {
-          image: "url",
+          image: "https://www.gonext.com/wp-content/uploads/2019/10/Portland-Oregon_iS_994318522_web.jpg",
           label: "Portland, OR",
           value: 1
         },
         {
-          image: "url",
+          image: "https://www.colorado.com/sites/default/files/styles/1000x685/public/Downtown%20Golden%20by%20Visit%20Golden.jpg?itok=2HLnfcK6",
           label: "Golden, Co",
           value: 2
         },
         {
-          image: "url",
+          image: "https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2020/08/omaha-nebraska-skyline-e1597251638318.jpg",
           label: "Omaha, Nebraska",
           value: 3
         },
         {
-          image: "url",
+          image: "https://media.architecturaldigest.com/photos/5da74823d599ec0008227ea8/16:9/w_2560%2Cc_limit/GettyImages-946087016.jpg",
           label: "New York City",
           value: 4
         }
@@ -109,22 +109,22 @@ export const surveyData = {
       title: 'Which music do you prefer?',
       choices: [
         {
-          image: "url",
+          image: "https://www.rollingstone.com/wp-content/uploads/2018/06/queen-news-of-the-world-opener-c5bda6ed-0bdf-42c0-8d93-7b29ec5c4259.jpg",
           label: "Alternative/Rock",
           value: 1
         },
         {
-          image: "url",
+          image: "http://socialistworker.org/sites/default/files/images/2018/01/arts-rock-cds-johnny-cash-folsom-prison.jpg",
           label: "Bluegrass/Folk",
           value: 2
         },
         {
-          image: "url",
+          image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2016%2F05%2Ftravel-like-beyonce0516.jpg&q=85",
           label: "Pop",
           value: 3
         },
         {
-          image: "url",
+          image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-86099434-1559917897.jpg",
           label: "R&B/Hip Hop",
           value: 4
         }
